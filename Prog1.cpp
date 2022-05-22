20130a03ebacb1591c22d9f3988099375dd901e7
Xavier Romero Hernández <100869394+Xavier-RH@users.noreply.github.com>
2022-05-22 10:37:46 -0500

    Add files via upload

---
+++ Prog1.cpp
@@ -0,0 +1,11 @@
+/* Autor: Xavier Romero Hernández, Realizado: 03/02/2022 
+	Primer programa en C, Hola mundo
+	*/
+
+#include <stdio.h>
+
+int main (){
+	printf("Hola mundo!!!\n\n");
+	printf("Xavier Romero Hernández");
+	return 0;
+}
